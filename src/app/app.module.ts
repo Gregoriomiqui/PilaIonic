69bc69659d42c5e14aee0556fae81b6cecc8280d
gregoriomiqui <gregoriomiqui@gmail.com>
2018-03-19 15:10:46 -0400

    Navegar de una Pagina a otra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ElementoPage } from  '../pages/elemento/elemento'
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ElementoPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ElementoPage
   ],
   providers: [
     StatusBar,
